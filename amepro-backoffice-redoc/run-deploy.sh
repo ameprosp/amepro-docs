041405a3f48be86420e0a6f7f090906ea343b72b
thiago.assis <thiago.assis@b2wdigital.com>
2019-06-26 10:03:22 -0300

    .

---
+++ amepro-backoffice-redoc/run-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "1 - Generate files."
+	npm run build > /dev/null
+echo "2 - Acess redoc deploy folder."
+	cd web_deploy
+echo "3 - Copy generated files to /doc folder"
+	currentDirectory=$(pwd)
+	cp -rf $currentDirectory"/." ../docs/backoffice
+echo "4 - Track files to gitHub"
+	cd ..
+	git add . 
+echo "5 - Commit Files. What is the commit's name?"
+	read commitName
+echo "6 - Commit Files"
+	git commit -m "$commitName
+	request-checks: false"
+echo "7 - Push to git"
+	git push
+echo "*********************************************
+Deployed sucessfully, files published on git.
+*********************************************"
---
+++ readme.md
@@ -0,0 +1,20 @@
+**How use it:**
+
+	1. Clone project
+	2. Open the cloned folder
+	3. Navigate to amepro-backoffice-redoc or amepro-company-redoc
+	4. type npm-start
+	
+	Follow the instructions in the screen
+
+	After that, you can deploy your documentation by running ./run-deploy.sh
+
+	**ATENTION**
+	Your cloned folder must be linked to a git repo before deploy. Use the command below to do that:
+	git remote add origin <YOUR GIT REPO URL>
+	
+**The documentations are avaliable in:**
+
+<https://thiago670.github.io/amepro-docs/backoffice/>
+
+<https://thiago670.github.io/amepro-docs/company/>
